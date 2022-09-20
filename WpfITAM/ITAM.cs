7fe3d994c35d0476832b602d112d1f8395c485e9
rbit <44907670+rbitzigeio@users.noreply.github.com>
2022-09-20 13:29:22 +0000

    Initial Load.
    Initial Load.

---
+++ WpfITAM/ITAM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class ITAM {
+	
+	private string _icto;
+    private string _name;
+    private string _adm;
+    private string _admVertreter;
+    private string _organisation;
+    private string _bdl;
+    private string _verteiler;
+
+
+    public ITAM(string icto) {_icto = icto;}
+
+	public string getIcto() {return _icto;}
+    public string getName() {return _name;}
+    public void setName(string name) {_name = name;}
+    public string getADM() { return _adm; }
+    public void setADM(string adm) { _adm = adm; }
+    public string getADMVertreter() { return _admVertreter; }
+    public void setADMVertreter(string admVertreter) { _admVertreter = admVertreter; }
+    public string getOrganisation() { return _organisation; }
+    public void setOrganisation(string organisation) { _organisation = organisation; }
+    public string getBDL() { return _bdl; }
+    public void setBDL(string bdl) { _bdl = bdl; }
+    public string getVerteiler() { return _verteiler; }
+    public void setVerteiler(string verteiler) { _verteiler = verteiler; }
+}
---
+++ WpfITAM/MainWindow.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
+using System.Windows.Shapes;
+
+namespace WpfITAM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedDictionary<string, ITAM> mITAM       = null;
+        Dictionary<string, string>     mNameToIcto = new Dictionary<string, string>();
+        string lastName = null;
+
+        public MainWindow() {
+            loadData(); 
+            InitializeComponent();
+        }
+
+        private void loadData() {
+            // Load IT-AM Data from csv file
+            string dir = "C:\\Users\\RBO-VS-Admin\\source\\repos\\WpfITAM_Old\\WpfITAM\\data\\";
+            ITAM icto;
+            // Read Azure-IT-AM data
+            Dictionary<string, ITAM> unsorted = new Dictionary<string, ITAM>();
+            foreach (string line in System.IO.File.ReadLines(dir + "Azure-IT-AM.csv")) {
+                //Trace.WriteLine(line);
+                icto = new ITAM(line.Trim());
+                unsorted.Add(line, icto);
+            }
+            mITAM = new SortedDictionary<string, ITAM>(unsorted);
+            // Read IT-AM-Application data
+            foreach (string line in System.IO.File.ReadLines(dir + "IT-AM-Applications.csv")) {
+                //Trace.WriteLine(line);
+                string[] s = line.Split(";");
+                if (s.Length > 2 && mITAM.ContainsKey(s[2])) {
+                    ITAM itam = mITAM[s[2]];
+                    itam.setName(s[1]);
+                    mNameToIcto.Add(s[1], s[2]);
+                    if (s.Length > 5){
+                        itam.setOrganisation(s[5]);
+                        itam.setADM(s[6]);
+                    }
+                }
+            }
+            // Read IT-AM-Zuständigkeiten data
+            foreach (string line in System.IO.File.ReadLines(dir + "IT-AM-Zuständigkeiten.csv")) {
+                //Trace.WriteLine(line);
+                string[] s = line.Split(";");
+                if (s.Length == 17) {
+                    if (mITAM.ContainsKey(s[3])) {
+                        ITAM itam = mITAM[s[3]];
+                        switch (s[10]) {
+                            case "BDL AP":             itam.setBDL(s[6]);           break;
+                            case "ADM-Vertreter":      itam.setADMVertreter(s[11]); break;
+                            case "BenachrichtigungCh": itam.setVerteiler(s[11]);    break;
+                            default: break;
+                        }
+                    }
+                }
+            }
+        }
+        private void TVLoader(object sender, RoutedEventArgs e) {
+            tbLog.Text = "Count of IT-AM objects : " + mITAM.Count().ToString();
+            // Create a TreeViewItem.
+            TreeViewItem item = new TreeViewItem();
+            item.Header = "IT-AM Sytem";
+            // Insert tree items to root item
+            foreach (var entry in mITAM) {
+                TreeViewItem tvi = new TreeViewItem();
+                tvi.Header = entry.Key;
+                item.Items.Add(tvi);
+            }
+            // Get TreeView reference and add  items.
+            var tree = sender as TreeView;
+            if (tree != null){
+                tree.Items.Add(item);
+            }
+        }
+
+        private void BtnExitClick(object sender, RoutedEventArgs e) {
+            tbLog.Text = "Exit Application";
+            Application.Current.MainWindow.Close();
+        }
+
+        /*--------------------------------------------------------------------
+         * Aktualisierung der Daten des IT-Systems bei Änderung der Auswahl im Baum
+         */
+        private void selectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e) {
+            var   item = (e.NewValue as TreeViewItem);
+            if (item != null) {
+                string icto = item.Header.ToString();
+                if (icto != null) { 
+                    tbLog.Text  = icto;
+                    if (mITAM.ContainsKey(icto)) {
+                        ITAM itam = mITAM[icto];
+                        lICTO.Content         = icto;
+                        //lName.Content         = itam.getName();
+                        tbName.Text           = itam.getName();
+                        lADM.Content          = itam.getADM();
+                        lADMVertreter.Content = itam.getADMVertreter();
+                        lOrganisation.Content = itam.getOrganisation();
+                        lVerteiler.Content    = itam.getVerteiler();
+                        lBDL.Content          = itam.getBDL();
+                    }
+                }
+            }
+        }
+        /*--------------------------------------------------------------------
+         * Extrahieren der gefundenen Daten in eine CSV Datei, die an Dienstleister
+         * übergeben werden kann.
+         */
+        private void BtnExtractClick(object sender, RoutedEventArgs e) {
+            tbLog.Text = "Extract Application";
+        }
+        /*--------------------------------------------------------------------
+         * Das Textfeld mit dem Namen des IT-Systems kann auch zur Suche genutzt werden.
+         * Nach Verlassen des Textfeldes, z.B. über <TAB> wird diese Methode aufgerufen
+         * und sucht über den IT-Systemname nach der ICTO Nummer. Diesewird bei erfolgreiche 
+         * Suche im Baum aktiviert und in den Textfeldern aktualisiert. 
+         * Wenn die Suche über den IT-System Namen keine ICTO Nummer findet,
+         * so erfolgt kein Update der Maske und die Daten der letzten korrekten 
+         * Suche werden unverändert angezeigt. 
+        */
+        private void tbName_LostFocus(object sender, RoutedEventArgs e) {
+            var tb = e.Source as TextBox;
+            if (mNameToIcto.ContainsKey(tb.Text)) {
+                tvIcto.Items.Clear();
+                string icto               = mNameToIcto[tb.Text];
+                tbLog.Text                = "Focus Lost. New ICTO : " + tb.Text + " / " + icto;
+                TreeViewItem newTVI       = new TreeViewItem();
+                newTVI.Header             = "IT-AM Sytem";
+                newTVI.IsExpanded         = true;
+                TreeViewItem selectedItem = new TreeViewItem();
+                tvIcto.Items.Add(newTVI);
+                foreach (var itam in mITAM) {
+                    TreeViewItem item2 = new TreeViewItem();
+                    item2.Header = itam.Value.getIcto();
+                    newTVI.Items.Add(item2);
+                    if (itam.Value.getIcto().Equals(icto)) {
+                        Trace.WriteLine("Icto found : " + icto);
+                        selectedItem = item2;
+                        
+                    }
+                }
+                selectedItem.SetValue(TreeViewItem.IsSelectedProperty, true);
+                tvIcto.UpdateLayout();
+            } else {
+                tbLog.Text = "ICTO for " + tb.Text + " not found.";
+                if (lastName != null) { 
+                    tb.Text = lastName; 
+                }
+            }  
+            
+        }
+        
+        /*--------------------------------------------------------------------
+         * Sichern des Namens des zuletzt korrekt ausgewählten IT-Systems
+         * Wenn die Suche über den IT-System Namen keine ICTO Nummer findet,
+         * so erfolgt kein Update der Maske und die Daten der letzten korrekten 
+         * Suche werden unverändert angezeigt. 
+        */
+        private void tbName_GotFocus(object sender, RoutedEventArgs e) {
+            var tb = e.Source as TextBox;
+            lastName = tb.Text;
+        }
+    }
+}
